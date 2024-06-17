04c774924158849a9cf6afa28d3b847ff5535ea1
Santiago Agustín Acosta <santiagoacosta777@gmail.com>
2024-06-17 10:23:39 -0300

    Time: 415 ms (100.00%) | Memory: 147.8 MB (100.00%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.dart
@@ -0,0 +1,31 @@
+//https://en.wikipedia.org/wiki/Sum_of_two_squares_theorem
+
+class Solution {
+  bool judgeSquareSum(int c) {
+    if (c == 0) {
+      return true;
+    }
+    while (c % 2 == 0) {
+      c ~/= 2;
+    }
+    if (c == 1) {
+      return true;
+    }
+    var i = 3;
+    while (i * i <= c) {
+      var count = 0;
+      while (c % i == 0) {
+        count++;
+        c ~/= i;
+      }
+      if (count % 2 == 1) {
+        return false;
+      }
+      while (c % (i + 2) == 0) {
+        c ~/= (i + 2);
+      }
+      i += 4;
+    }
+    return c % 4 != 3;
+  }
+}
