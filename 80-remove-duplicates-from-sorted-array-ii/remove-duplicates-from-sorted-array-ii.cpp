3c7e849887dda41fc1ac745d0747dcb6b35da9c6
Santiago Agustín Acosta <santiagoacosta777@gmail.com>
2024-06-25 16:52:30 -0300

    Time: 0 ms (100.00%) | Memory: 13.3 MB (76.20%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -1,23 +1,24 @@
-#include <map>
 #include <vector>
 
 class Solution {
 public:
     int removeDuplicates(std::vector<int>& nums) {
-        auto numCount = std::map<int, int>();
-        auto read = nums.begin();
+        auto prevNumread = nums[0] - 1;
+        auto prevNumread2 = nums[0] - 2;
         auto write = nums.begin();
+        auto read = nums.begin();
         auto k = 0;
         while(read != nums.end()) {
-            if(numCount[*read] <= 1) {
-                numCount[*read]++;
+            if(prevNumread == prevNumread2 && *read == prevNumread) {
+                read++;
+            }
+            else {
+                prevNumread2 = prevNumread;
+                prevNumread = *read;
                 *write = *read;
                 write++;
                 read++;
                 k++;
-            }
-            else {
-                read++;
             }
         }
         return k;
